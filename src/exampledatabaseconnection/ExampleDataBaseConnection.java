409c6ff62faedd599d584d4640d9db9633bbe3c9
lilitaki <33423990+lilitaki@users.noreply.github.com>
2017-12-02 11:48:24 +0200

    Update ExampleDataBaseConnection.java

--- src/exampledatabaseconnection/ExampleDataBaseConnection.java
+++ src/exampledatabaseconnection/ExampleDataBaseConnection.java
@@ -18,7 +18,7 @@ public class ExampleDataBaseConnection {
        static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
        static final String DB_URL = "jdbc:mysql://localhost/katalogos";
        static final String USER = "root";
-       static final String PASS = "pitsakos14";
+       static final String PASS = "";
 
     public static void main(String[] args) {
         
